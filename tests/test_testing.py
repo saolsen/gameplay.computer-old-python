ac02ec68684565fe492f7df8de9ac0c17a797143
steve olsen <steve@steve.computer>
2023-04-07 11:44:59 -0500

    fix it

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -2,14 +2,6 @@ import os
 import pytest
 
 
-def test_probs():
-    assert False
-
-
-def test_testing():
-    assert True
-
-
 @pytest.mark.skipif(
     not os.getenv("GITHUB_ACTIONS"),
     reason="Only run in CI. Locally you'll have all deps so this will fail.",
